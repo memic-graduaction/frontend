c0d24488a4cac3ec7fc407b9e667f99d7b7bdd88
ming0o <isha3992@gmail.com>
2024-01-31 11:35:37 +0900

    Refactor: 유효성 검사 중복 함수 수정 및 리팩토링

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// HomePage.tsx
+import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { youtubeLinkState } from 'src/recoil/states';
 import Header from '../../components/Header/Header';
@@ -8,42 +8,49 @@ import SearchButton from './SearchButton';
 import mouseIcon from '../../assets/mouseIcon.png';
 
 function HomePage() {
-  const navigator = useNavigate();
   const setLink = useSetRecoilState(youtubeLinkState);
+  const [inputValue, setInputValue] = useState('');
   const [isValidUrl, setIsValidUrl] = useState(false);
+  const [isYoutubeUrl, setIsYoutubeUrl] = useState(false);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    try {
-      const userEnteredUrl = e.target.value;
-      const url = new URL(userEnteredUrl);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
 
-      const urlParams = url.searchParams;
-      const param = String(urlParams.get('v'));
-      setLink(param);
+    // URL 및 YouTube 동영상 URL 형식 검사
+    const url = new URL(e.target.value);
+    setIsYoutubeUrl(
+      url.hostname.includes('youtube') &&
+      url.pathname.includes('watch') &&
+      url.searchParams.has('v')
+    );
 
-      // URL이 유효하면 상태를 true로 설정합니다.
-      setIsValidUrl(true);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`유효하지 않은 URL: ${e.target.value}`);
-      // 상태를 false로 설정하여 유효하지 않은 상태임을 알립니다.
-      setIsValidUrl(false);
+    // URL이 유효한지 검사
+    setIsValidUrl(true);
+  };
+
+  const handleButtonClick = () => {
+    // 버튼을 누를 때 실행되는 함수
+    if (isValidUrl && isYoutubeUrl) {
+      const url = new URL(inputValue);
+      const videoId = url.searchParams.get('v');
+      setLink(videoId);
+      console.log('유효한 YouTube URL입니다.');
+    } else {
+      alert('유효한 YouTube 동영상 URL이 아닙니다.');
     }
   };
-  const handleClick = () => {
-    navigator('/script');
-  };
+
   return (
     <S.Layout>
       <Header />
       <S.Title>
-        좋아하는 동영상<S.T>으로</S.T><br/>
+        좋아하는 동영상<S.T>으로</S.T><br />
         영어 공부<S.T>와</S.T> 회화 능력 향상<S.T>까지</S.T>
       </S.Title>
       <S.SearchBar>
         <S.Guide>SEARCH</S.Guide>|
         <S.Input placeholder="YOUTUBE 주소를 붙여넣기 해주세요" onChange={handleChange} />
-        <SearchButton onClick={handleClick} isValidUrl={isValidUrl} />
+        <SearchButton onClick={handleButtonClick} isValidUrl={isValidUrl} isYoutubeUrl={isYoutubeUrl} />
       </S.SearchBar>
       <S.ScrollIcon src={mouseIcon} alt="Icon" />
     </S.Layout>
--- src/pages/HomePage/SearchButton.tsx
+++ src/pages/HomePage/SearchButton.tsx
@@ -1,27 +1,32 @@
+// SearchButton.tsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import searchIcon from '../../assets/searchIcon.png';
 
 interface SearchButtonProps {
-    onClick: () => void;
-    isValidUrl: boolean;
+  onClick: () => void;
+  isValidUrl: boolean;
+  isYoutubeUrl: boolean;
 }
 
-function SearchButton({ onClick, isValidUrl }: SearchButtonProps) {
-    const isYoutubeUrl = (url: string): boolean => url.includes("youtube.com") && url.includes("watch?v=");
+function SearchButton({ onClick, isValidUrl, isYoutubeUrl }: SearchButtonProps) {
+  const navigator = useNavigate();
 
-    const handleButtonClick = () => {
-        if (isValidUrl && isYoutubeUrl(window.location.href)) {
-            onClick();
-        } else {
-            alert("유효한 YouTube URL이 아닙니다.");
-        }
-    };
+  const handleButtonClick = () => {
+    if (isValidUrl && isYoutubeUrl) {
+      onClick();
+      navigator('/script');
+      
+    } else {
+      alert("유효한 YouTube URL이 아닙니다.");
+    }
+  };
 
-    return (
-        <button type="submit" onClick={handleButtonClick}>
-            <img style={{ width: '2.5rem' }} src={searchIcon} alt="Search Icon" />
-        </button>
-    );
+  return (
+    <button type="submit" onClick={handleButtonClick}>
+      <img style={{ width: '2.5rem' }} src={searchIcon} alt="Search Icon" />
+    </button>
+  );
 }
 
 export default SearchButton;
