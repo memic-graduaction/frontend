1a03e020398637d4808e4dcc8909e4322421ef3c
eomseona <eomsa98@tukorea.ac.kr>
2024-02-25 11:57:11 +0900

    feat : 사용자 음성 녹음, 정지, 재생 기능 구현

---
+++ src/pages/RecordPage/RecordPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecordModal from 'src/components/RecordModal/RecordModal';
+
+function RecordPage() {
+  return (
+    <div>
+      <h1>React Media Recorder</h1>
+      <RecordModal />
+    </div>
+  );
+}
+
+export default RecordPage;
