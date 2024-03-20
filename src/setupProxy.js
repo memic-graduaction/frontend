5b17b888fb3128b19f1ef4afb5a8e78ce13d090f
eomseona <eomsa98@tukorea.ac.kr>
2024-03-20 14:14:48 +0900

    docs: ffmpeg 라이브러리 설치 삭제

--- src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-  app.use((request, response, next) => {
-    response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-    next();
-  });
-};
