0af49434fad94a19af90e428cfe07e2870970508
eomseona <eomsa98@tukorea.ac.kr>
2024-05-03 19:35:33 +0900

    feat: 드래그 오른쪽으로만 가능하게 설정

--- src/utils/getSelectedPhrase.ts
+++ src/utils/getSelectedPhrase.ts
@@ -1,19 +1,31 @@
 export const getSelectedPhrase = () => {
-  const obj = window.getSelection();
-  const phrase = obj.toString();
-  const startIndex = obj.anchorOffset;
-  const endIndex = obj.focusOffset;
-  const range = obj.getRangeAt(0);
-  const content = range.extractContents();
-  console.log(`${startIndex}: 스타트`);
-  console.log(`${endIndex}:앤드`);
-  console.log(`${phrase}: 문장`);
+  // 선택된 범위에 selection 객체 생성
+  const sel = window.getSelection();
+  // 범위 객체
+  const range = sel.getRangeAt(0);
+  // 선택된 텍스트
+  const phrase = sel.toString();
+  // 시작 지점 인덱스
+  const startIndex = sel.anchorOffset;
+  // 끝나는 지점의 인덱스
+  const endIndex = sel.focusOffset;
+
+  let content = null;
+
+  if (startIndex > endIndex) sel.removeAllRanges();
+  else content = range.extractContents();
+
+  // 범위를 startIndex부터 endIndex로 설정해야함
 
   // 선택된 텍스트 스타일 변경
-  const span = document.createElement('span');
-  span.appendChild(content);
-  span.style.background = '#FFE9B0';
-  range.insertNode(span);
+  if (content) {
+    const span = document.createElement('span');
+    span.appendChild(content);
+    span.style.background = '#FFE9B0';
+    range.insertNode(span);
+  }
 
   return { phrase, startIndex, endIndex };
 };
+
+//
