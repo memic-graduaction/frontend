a3be703dd2735c70fe5726b7d68bd6595e9ccb3d
ming0o <isha3992@gmail.com>
2024-06-04 01:12:38 +0900

    feat: 날짜 api 연결

--- src/components/MyPage/Dashboard/WordsGraph.tsx
+++ src/components/MyPage/Dashboard/WordsGraph.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 // import { ResponsiveLine } from '@nivo/line';
-// import axios from 'axios';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { selectedDateState, UUid } from '../../../recoil/states';
 
 function App() {
+  const selectedDate = useRecoilValue(selectedDateState);
+  const user = useRecoilValue(UUid);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const year = selectedDate.getFullYear();
+      const month = selectedDate.getMonth() + 1;
+
+      try {
+        const response = await axios.get(`/recognized-sentences/counts?year=${year}&month=${month}`, {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`, // UUid에서 가져온 accessToken 사용
+          },
+        });
+        setData(response.data);
+        console.log(setData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    if (user.accessToken) {
+      fetchData(); // 토큰이 있을 때만 fetchData 호출
+    }
+  }, [selectedDate, user.accessToken]);
+
   return (
     <Container>
-      ㅎㅇㅎㅇ
+      <DataList>
+        {data.map((item) => (
+          <DataItem key={item.date}>
+            Date: {item.date}, Count: {item.count}
+          </DataItem>
+        ))}
+      </DataList>
     </Container>
   );
 }
@@ -21,4 +55,16 @@ const Container = styled.div`
   background: #fff;
   border-radius: 20px;
   padding: 30px;
+`;
+
+const DataList = styled.div`
+  margin-top: 20px;
+`;
+
+const DataItem = styled.div`
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  color: #000000;
 `;
