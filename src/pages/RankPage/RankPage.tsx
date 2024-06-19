256e6c471db184ec147cb489db0bba901cd529cc
eomseona <eomsa98@tukorea.ac.kr>
2024-06-19 20:01:20 +0900

    style: 추천 영상 페이지 퍼블리싱

--- src/pages/RankPage/RankPage.tsx
+++ src/pages/RankPage/RankPage.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import * as S from './Styles';
-import pic1 from '../../assets/pic1.png';
-import pic2 from '../../assets/pic2.png';
-import pic3 from '../../assets/pic3.png';
 
 function RankPage() {
-
   return (
     <div id="rankPage">
       <S.Layout>
         <Header />
-        <S.Title>
-          오늘의 추천 동영상
-        </S.Title>
         <S.Container>
-          <S.Preview src={pic1} alt="Image 1" />
-          <S.Preview src={pic2} alt="Image 2" />
-          <S.Preview src={pic3} alt="Image 3" />
+          <S.Title>오늘은 이런 영상 어때요?</S.Title>
+          <S.ThumbnailBox>
+            <S.Thumbnail href="https://www.youtube.com/watch?v=rR4n-0KYeKQ" />
+            <S.Thumbnail href="https://www.youtube.com/watch?v=rR4n-0KYeKQ" />
+            <S.Thumbnail href="https://www.youtube.com/watch?v=rR4n-0KYeKQ" />
+          </S.ThumbnailBox>
         </S.Container>
       </S.Layout>
     </div>
